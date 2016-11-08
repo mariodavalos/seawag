62a6ae84180591c6dd790cfb69824d3c3d01bfbb
Mario Dávalos <mario@evelinas-imac.local>
2016-11-08 12:16:59 -0600

    Initial Commit

---
+++ seawag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  seawag
+//
+//  Created by Mario Dávalos on 08/11/16.
+//  Copyright © 2016 Ooomovil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
