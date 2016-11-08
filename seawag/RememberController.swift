70f615e60e8e0c5ea9394642772c685c5cb0dd5d
Mario Fernando Davalos Navarro <mariofernandodavalos@hotmail.com>
2016-11-08 17:30:56 -0600

    1.0.1

---
+++ seawag/RememberController.swift
@@ -0,0 +1,25 @@
+//
+//  RememberController.swift
+//  seawag
+//
+//  Created by Mario Dávalos on 08/11/16.
+//  Copyright © 2016 Ooomovil. All rights reserved.
+//
+
+import UIKit
+
+class RememberController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- seawag/ViewController.swift
+++ seawag/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Login: UIButton!
+    @IBOutlet weak var Registrate: UIButton!
+    @IBOutlet weak var Facebook: UIButton!
+    @IBOutlet weak var User: UITextField!
+    @IBOutlet weak var Paser: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.Login.layer.cornerRadius = 22.0
+        self.Registrate.layer.cornerRadius = 22.0
+        self.Registrate.layer.borderWidth = 2
+        self.Registrate.layer.borderColor = UIColor.white.cgColor
+        self.Facebook.layer.cornerRadius = 22.0
+        self.User.layer.cornerRadius = 22.0
+        self.User.layer.borderWidth = 2
+        self.User.layer.borderColor = UIColor.white.cgColor
+        self.Paser.layer.cornerRadius = 22.0
+        self.Paser.layer.borderWidth = 2
+        self.Paser.layer.borderColor = UIColor.white.cgColor
+        
     }
 
     override func didReceiveMemoryWarning() {
